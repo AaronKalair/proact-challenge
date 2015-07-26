c42c4adc085fe1bcc7f610aa759d5449a131347f
Aaron Kalair <aaronkalair@gmail.com>
2015-07-26 11:18:21 +0100

    Add local_settings override

---
+++ local_settings.py
@@ -0,0 +1,4 @@
+HOST = "machinelearningdb.c96ee52wn0af.us-west-2.rds.amazonaws.com"
+USERNAME = "dbroot"
+PASSWORD = ""
+DATABASE = "mldb"
--- settings.py
+++ settings.py
@@ -2,3 +2,9 @@ HOST = "localhost"
 USERNAME = "root"
 PASSWORD = ""
 DATABASE = "als"
+
+try:
+    from local_settings import *
+except:
+    print "No local_settings.py found"
+    pass
